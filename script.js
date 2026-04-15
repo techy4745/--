fe832809258db8459dcca879e0447429dcca2586
techy <himanshu766538@gmail.com>
2026-04-15 18:55:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("openBtn").onclick = function() {
+    document.getElementById("surprise").classList.remove("hidden");
+
+    document.getElementById("blurLayer").style.display = "none";
+
+    this.style.display = "none";
+}
